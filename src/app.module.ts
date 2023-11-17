b69065cee4a12b72b06d379b667cb9ef35871109
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-11-17 18:54:59 +0100

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', // Explicitly specify the driver
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'authapp',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
